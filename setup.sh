0aa75085fead644d43625f71b21708fa8bebbf3f
Mykola Morgunenko <Olli25010@gmail.com>
2023-03-21 22:35:53 +0100

    initial commit

--- setup.sh
+++ setup.sh
@@ -6,11 +6,14 @@
 #
 # yet :TODO
 
+echo "Password needed to create correct symlinks and make it possible to run test scripts from any directory"
 sudo ln -sf $PWD/general/run_prog_with_time_bound.sh /usr/local/bin/run_prog_with_time_bound
 sudo chmod +x /usr/local/bin/run_prog_with_time_bound
 
 sudo ln -sf $PWD/acs/integral.sh /usr/local/bin/test_integral
 sudo chmod +x /usr/local/bin/test_integral
+
+echo "Now do \"source ~/.bashrc\" or \"source ~/.zshrc\" and use 'test_integral' from any location" 
 
 # while true; do
 # 	[[ -t 0 ]] && {read -t 10 -n 2 -p $'\e[1;32mSet up for? [y/n] (default: y)\e[0m\n' resp || resp=y;}
